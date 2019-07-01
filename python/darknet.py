65610b333682df682f430224c623a4bdb2d2ce14
mysafesky <mysafesky@gmail.com>
2019-07-01 01:27:14 -0700

    remove weights

--- python/darknet.py
+++ python/darknet.py
@@ -1,6 +1,7 @@
 from ctypes import *
 import math
 import random
+from os.path import expanduser
 
 def sample(probs):
     s = sum(probs)
@@ -42,115 +43,136 @@ class METADATA(Structure):
     _fields_ = [("classes", c_int),
                 ("names", POINTER(c_char_p))]
 
-    
+class Yolo:
 
-#lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
-lib = CDLL("libdarknet.so", RTLD_GLOBAL)
-lib.network_width.argtypes = [c_void_p]
-lib.network_width.restype = c_int
-lib.network_height.argtypes = [c_void_p]
-lib.network_height.restype = c_int
+    def __init__(self):
+        print("1\n")
+        self.lib = CDLL("libdarknet.so", RTLD_GLOBAL)
+        self.lib.network_width.argtypes = [c_void_p]
+        self.lib.network_width.restype = c_int
+        self.lib.network_height.argtypes = [c_void_p]
+        self.lib.network_height.restype = c_int
 
-predict = lib.network_predict
-predict.argtypes = [c_void_p, POINTER(c_float)]
-predict.restype = POINTER(c_float)
+        self.predict = self.lib.network_predict
+        self.predict.argtypes = [c_void_p, POINTER(c_float)]
+        self.predict.restype = POINTER(c_float)
 
-set_gpu = lib.cuda_set_device
-set_gpu.argtypes = [c_int]
+        self.set_gpu = self.lib.cuda_set_device
+        self.set_gpu.argtypes = [c_int]
 
-make_image = lib.make_image
-make_image.argtypes = [c_int, c_int, c_int]
-make_image.restype = IMAGE
+        self.make_image = self.lib.make_image
+        self.make_image.argtypes = [c_int, c_int, c_int]
+        self.make_image.restype = IMAGE
 
-get_network_boxes = lib.get_network_boxes
-get_network_boxes.argtypes = [c_void_p, c_int, c_int, c_float, c_float, POINTER(c_int), c_int, POINTER(c_int)]
-get_network_boxes.restype = POINTER(DETECTION)
+        self.get_network_boxes = self.lib.get_network_boxes
+        self.get_network_boxes.argtypes = [c_void_p, c_int, c_int, c_float, c_float, POINTER(c_int), c_int, POINTER(c_int)]
+        self.get_network_boxes.restype = POINTER(DETECTION)
 
-make_network_boxes = lib.make_network_boxes
-make_network_boxes.argtypes = [c_void_p]
-make_network_boxes.restype = POINTER(DETECTION)
+        self.make_network_boxes = self.lib.make_network_boxes
+        self.make_network_boxes.argtypes = [c_void_p]
+        self.make_network_boxes.restype = POINTER(DETECTION)
 
-free_detections = lib.free_detections
-free_detections.argtypes = [POINTER(DETECTION), c_int]
+        self.free_detections = self.lib.free_detections
+        self.free_detections.argtypes = [POINTER(DETECTION), c_int]
 
-free_ptrs = lib.free_ptrs
-free_ptrs.argtypes = [POINTER(c_void_p), c_int]
+        self.free_ptrs = self.lib.free_ptrs
+        self.free_ptrs.argtypes = [POINTER(c_void_p), c_int]
 
-network_predict = lib.network_predict
-network_predict.argtypes = [c_void_p, POINTER(c_float)]
+        self.network_predict = self.lib.network_predict
+        self.network_predict.argtypes = [c_void_p, POINTER(c_float)]
 
-reset_rnn = lib.reset_rnn
-reset_rnn.argtypes = [c_void_p]
+        self.reset_rnn = self.lib.reset_rnn
+        self.reset_rnn.argtypes = [c_void_p]
 
-load_net = lib.load_network
-load_net.argtypes = [c_char_p, c_char_p, c_int]
-load_net.restype = c_void_p
+        self.load_net = self.lib.load_network
+        self.load_net.argtypes = [c_char_p, c_char_p, c_int]
+        self.load_net.restype = c_void_p
 
-do_nms_obj = lib.do_nms_obj
-do_nms_obj.argtypes = [POINTER(DETECTION), c_int, c_int, c_float]
+        self.do_nms_obj = self.lib.do_nms_obj
+        self.do_nms_obj.argtypes = [POINTER(DETECTION), c_int, c_int, c_float]
 
-do_nms_sort = lib.do_nms_sort
-do_nms_sort.argtypes = [POINTER(DETECTION), c_int, c_int, c_float]
+        self.do_nms_sort = self.lib.do_nms_sort
+        self.do_nms_sort.argtypes = [POINTER(DETECTION), c_int, c_int, c_float]
 
-free_image = lib.free_image
-free_image.argtypes = [IMAGE]
+        self.free_image = self.lib.free_image
+        self.free_image.argtypes = [IMAGE]
 
-letterbox_image = lib.letterbox_image
-letterbox_image.argtypes = [IMAGE, c_int, c_int]
-letterbox_image.restype = IMAGE
+        self.letterbox_image = self.lib.letterbox_image
+        self.letterbox_image.argtypes = [IMAGE, c_int, c_int]
+        self.letterbox_image.restype = IMAGE
 
-load_meta = lib.get_metadata
-lib.get_metadata.argtypes = [c_char_p]
-lib.get_metadata.restype = METADATA
+        self.load_meta = self.lib.get_metadata
+        self.lib.get_metadata.argtypes = [c_char_p]
+        self.lib.get_metadata.restype = METADATA
 
-load_image = lib.load_image_color
-load_image.argtypes = [c_char_p, c_int, c_int]
-load_image.restype = IMAGE
+        self.load_image = self.lib.load_image_color
+        self.load_image.argtypes = [c_char_p, c_int, c_int]
+        self.load_image.restype = IMAGE
 
-rgbgr_image = lib.rgbgr_image
-rgbgr_image.argtypes = [IMAGE]
+        self.rgbgr_image = self.lib.rgbgr_image
+        self.rgbgr_image.argtypes = [IMAGE]
 
-predict_image = lib.network_predict_image
-predict_image.argtypes = [c_void_p, IMAGE]
-predict_image.restype = POINTER(c_float)
+        self.predict_image = self.lib.network_predict_image
+        self.predict_image.argtypes = [c_void_p, IMAGE]
+        self.predict_image.restype = POINTER(c_float)
 
-def classify(net, meta, im):
-    out = predict_image(net, im)
-    res = []
-    for i in range(meta.classes):
-        res.append((meta.names[i], out[i]))
-    res = sorted(res, key=lambda x: -x[1])
-    return res
-
-def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
-    im = load_image(image, 0, 0)
-    num = c_int(0)
-    pnum = pointer(num)
-    predict_image(net, im)
-    dets = get_network_boxes(net, im.w, im.h, thresh, hier_thresh, None, 0, pnum)
-    num = pnum[0]
-    if (nms): do_nms_obj(dets, num, meta.classes, nms);
-
-    res = []
-    for j in range(num):
+    def classify(self,net, meta, im):
+        out = self.predict_image(net, im)
+        res = []
         for i in range(meta.classes):
-            if dets[j].prob[i] > 0:
-                b = dets[j].bbox
-                res.append((meta.names[i], dets[j].prob[i], (b.x, b.y, b.w, b.h)))
-    res = sorted(res, key=lambda x: -x[1])
-    free_image(im)
-    free_detections(dets, num)
-    return res
+            res.append((meta.names[i], out[i]))
+        res = sorted(res, key=lambda x: -x[1])
+        return res
+
+    def detect(self,image, net = None,meta = None, thresh=.5, hier_thresh=.5, nms=.45):
+        if net is None:
+            net = self.net
+        
+        if meta is None:
+            meta = self.meta
+
+        im = self.load_image(image.encode(), 0, 0)
+        num = c_int(0)
+        pnum = pointer(num)
+        self.predict_image(net, im)
+        dets = self.get_network_boxes(net, im.w, im.h, thresh, hier_thresh, None, 0, pnum)
+        num = pnum[0]
+        if (nms): self.do_nms_obj(dets, num, meta.classes, nms)
+
+        res = []
+        for j in range(num):
+            for i in range(meta.classes):
+                if dets[j].prob[i] > 0:
+                    b = dets[j].bbox
+                    res.append((meta.names[i], dets[j].prob[i], (b.x, b.y, b.w, b.h)))
+        res = sorted(res, key=lambda x: -x[1])
+        self.free_image(im)
+        self.free_detections(dets, num)
+        return res
     
+    def set_net(self,path_cfg, path_weight):
+        self.net = self.load_net(path_cfg.encode(),path_weight.encode(), 0)
+    
+    def set_meta(self,path_meta):
+        self.meta = self.load_meta(path_meta.encode())
+
+
+# TODO: Translation between different label representation
+
+# TODO: Visualization
+
+
+# Due to weird behavior of relative directory
+# Absolute path should be used everywhere including meta setting
+
 if __name__ == "__main__":
-    #net = load_net("cfg/densenet201.cfg", "/home/pjreddie/trained/densenet201.weights", 0)
-    #im = load_image("data/wolf.jpg", 0, 0)
-    #meta = load_meta("cfg/imagenet1k.data")
-    #r = classify(net, meta, im)
-    #print r[:10]
-    net = load_net("cfg/tiny-yolo.cfg", "tiny-yolo.weights", 0)
-    meta = load_meta("cfg/coco.data")
-    r = detect(net, meta, "data/dog.jpg")
-    print r
+    print("Start to initialize Darknet\n")
+    yolo = Yolo()
+    print("Start to set net\n")
+    yolo.set_net("../cfg/yolov3.cfg","../weights/yolov3.weights")
+    print("Start to set meta\n")
+    yolo.set_meta(expanduser("~")+"/tools/yolo/darknet/cfg/coco.data")
+    r = yolo.detect(expanduser("~")+"/tools/yolo/darknet/data/dog.jpg")
+    print(r)
     
 
